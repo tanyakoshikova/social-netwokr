18b7ff8c79007d1c4c87c0533f3996105733f38e
Tatyana <tkoshikova@gmail.com>
2023-03-30 16:26:32 +0700

    Форма для авторизации пользователя.

---
+++ src/components/Login/login.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import {reduxForm} from "redux-form";
+import {createField, Input} from "../common/FormsControls/FormsControls";
+import {required} from "../../utils/validators";
+import {connect} from "react-redux";
+import {login} from "../../redux/auth_reducer";
+import {Navigate} from "react-router-dom";
+import s from "./../common/FormsControls/FormsControls.module.css"
+
+
+const LoginForm = ( {handleSubmit, error, captchaUrl} ) => {
+    return (
+            <form onSubmit={handleSubmit}>
+                {createField("Email","email", [required], Input) }
+                {createField("Password","password", [required], Input, {type:"password"}) }
+                {createField(null,"rememberMe", [], Input, {type:"checkbox"}, "remember me") }
+
+                {captchaUrl && <img src={captchaUrl} alt={"captcha"}/>}
+                {captchaUrl &&  createField("Symbols from image", "captcha", [required], Input, {} )  }
+
+                {error && <div className={s.formSummaryError}>
+                    {error}
+                </div>
+                }
+                <div>
+                    <button>Login</button>
+                </div>
+            </form>
+    )
+}
+
+const LoginReduxForm = reduxForm( {form: 'login'} ) (LoginForm)
+
+const Login = (props) => {
+    const onSubmit = (formData) => {
+        props.login(formData.email, formData.password, formData.rememberMe, formData.captcha);
+    }
+
+    if (props.isAuth) {
+        return <Navigate to={"/profile"} />
+    }
+
+    return (
+        <div>
+            <h1>Login</h1>
+            <LoginReduxForm onSubmit={onSubmit} captchaUrl={props.captchaUrl} />
+        </div>
+    )
+}
+
+const mapStateToProps = (state) => ({
+    captchaUrl: state.auth.captchaUrl,
+    isAuth: state.auth.isAuth
+})
+export default connect(mapStateToProps, {login} ) (Login);
