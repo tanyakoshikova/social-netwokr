d27e6c001399feef0306f40e67d975da2a6279a6
Tatyana <tkoshikova@gmail.com>
2023-03-30 16:21:36 +0700

    HeaderContainer.jsx компонента, отрисовываетинформацию о пользователе, залогинин ли он.

---
+++ src/components/Header/HeaderContainer.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Header from "./Header";
+import {connect} from "react-redux";
+import {logout} from "../../redux/auth_reducer";
+import {compose} from "redux";
+
+
+class HeaderContainer extends React.Component {
+
+    render () {
+    return <Header {...this.props}/>
+    }
+}
+
+const mapStateToProps = (state) => ({
+    isAuth: state.auth.isAuth,
+    login: state.auth.login,
+});
+
+
+export default compose (
+    connect (mapStateToProps, {logout}) )(HeaderContainer);
+
